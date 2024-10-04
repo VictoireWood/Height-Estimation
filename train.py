c86e0684b57ba40206455989692546fba24e1d1a
Xingyu Shao <starainsxy28@163.com>
2024-10-04 13:24:24 +0800

    Revert "部分变量的值改动。"
    This reverts commit 4b4a27a834d2addd2e9b476b09fa74199aa1a8f1.

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ from utils.inference import inference
 # from eval import resume_info
 
 resume_info = {
-    'resume_model': False,
+    'resume_model': True,
     'resume_model_path': './logs/HE-dinov2_vitb14-MixVPR/2024-09-10_11-29-15/best_model.pth',
     'resume_train': False,
     'resume_train_path': './logs/HE-dinov2_vitb14-MixVPR/2024-09-10_11-29-15/last_checkpoint.pth',
